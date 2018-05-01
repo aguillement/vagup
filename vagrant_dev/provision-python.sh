9915bc5676f0f281a337903e35b76e9fa07b1e75
adrien.guillement <adrien.guillement@gmail.com>
2018-05-01 13:10:42 +0200

    split requirements

--- vagrant_dev/provision-python.sh
+++ vagrant_dev/provision-python.sh
@@ -5,7 +5,7 @@ cd /srv/vagup
 # requirements
 source ~/venv-vagup/bin/activate \
 && cd /srv/vagup/src/vagup \
-&& pip3.6 install -r requirements.txt \
+&& pip3.6 install -r /srv/vagup/src/requirements/base.txt \
 && python manage.py makemigrations \
 && python manage.py migrate \
 && echo "from django.contrib.auth.models import User; User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'admin')" | python manage.py shell \
