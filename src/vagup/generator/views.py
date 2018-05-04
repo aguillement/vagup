b5154547b92513955d197f1d44f3fb58d5db4ede
adrien.guillement <adrien.guillement@gmail.com>
2018-05-04 21:35:30 +0200

    fix css

--- src/vagup/generator/views.py
+++ src/vagup/generator/views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
+from datetime import datetime
+from django.shortcuts import render, render_to_response
 
 
 def index(request):
-    return HttpResponse("Hello, world.")
+    return render_to_response('index.html')
+
+def generator(request):
+    return render(request, 'generator/generate.html')
--- src/vagup/vagup/urls.py
+++ src/vagup/vagup/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url('^generator/', include('generator.urls')),
+    url('^', include('generator.urls')),
     url(r'^admin/', admin.site.urls),
 ]
