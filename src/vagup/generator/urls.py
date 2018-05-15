2c09e594fccb99d96e6f9da1c01af7cb1084358e
AdrienGuillement <adrien.guillement@gmail.com>
2018-05-15 13:26:51 +0200

    download button - first step

--- src/vagup/generator/urls.py
+++ src/vagup/generator/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     url('^$', views.index, name='index'),
-    url('^generator/', views.generator, name='generator')
+    url('^generator/', views.generator, name='generator'),
+    url('^generated/', views.generated, name='generated')
 ]
--- src/vagup/generator/views.py
+++ src/vagup/generator/views.py
@@ -1,10 +1,24 @@
+import os
+
+from django.conf import settings
 from django.http import HttpResponse
-from datetime import datetime
 from django.shortcuts import render, render_to_response
+
+from datetime import datetime
 
 
 def index(request):
     return render_to_response('index.html')
 
+
 def generator(request):
     return render(request, 'generator/generate.html')
+
+
+def generated(request):
+    filename = "./views.py"
+    with open(filename, 'rb') as f:
+        response = HttpResponse(f.read(), content_type='application/text')
+        response['Content-Disposition'] = 'attachment; filename=' + filename
+        response['Content-Type'] = 'application/text; charset=utf-8'
+        return response
